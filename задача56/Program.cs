7929785e2fdbdec4fa5ec9c47345056412e95942
EfimovSergeyA <116340503+EfimovSergeyA@users.noreply.github.com>
2022-12-10 12:32:01 +0300

    My homework8

---
+++ задача56/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int row = GetNumberFromUser("Введите количество строк: ","Ошибка ввода!");
+int col = GetNumberFromUser("Введите количество столбцов: ","Ошибка ввода!");
+int[,] matrix = GetArray(row, col, 0, 10);
+PrintArray(matrix);
+Console.WriteLine();
+
+int[] minSum = FindRowWithMinElementSum(matrix, 10);
+Console.WriteLine($"Минимальная сумма элементов {minSum[0]} содержится в {minSum[1]}-й строке");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);;
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+             Console.Write($"{inArray[i, j]} ");
+        }
+         Console.WriteLine();
+     }
+}
+
+int[] FindRowWithMinElementSum(int[,] array, int maxValue)
+{
+    int[] result = new int[2];   
+    int rowSum = 0;
+    result[0] = maxValue * array.GetLength(1);   
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            rowSum += array[i, j];
+        }
+        if (rowSum < result[0]) 
+        {
+            result[0] = rowSum;
+            result[1] = i;
+        }
+        rowSum = 0;
+    }
+    return result;
+}
